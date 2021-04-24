9259d92c3f92c6241fe8c1028cdb59b8b856fd12
root <root@eve-ng.example.com>
2021-04-24 09:12:48 +0530

    added ticket view to alarms plugin

--- plugins/alarmsview/server/types.ts
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface AlarmsviewPluginSetup {}
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface AlarmsviewPluginStart {}
